a1187fb06cde1554086f22f46952dbaed144a724
ITKaty <117355327+chabanova-ekaterina@users.noreply.github.com>
2023-05-11 09:42:44 +0300

    variables

---
+++ variables.py
@@ -0,0 +1,4 @@
+name = input('Your name: ')
+lastname = input('Your lastname: ')
+balance = int(input('Enter your balance: '))
+print(f"Уважаем(ый/ая) {name} {lastname}, Ваш баланс составляет {balance} $")
