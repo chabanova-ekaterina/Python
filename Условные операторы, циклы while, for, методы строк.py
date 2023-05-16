c06d0b57cf82940a4e11e4004f41670c371de47c
ITKaty <117355327+chabanova-ekaterina@users.noreply.github.com>
2023-05-16 11:17:40 +0300

    Update Условные операторы, циклы while, for, методы строк.py

--- Условные операторы, циклы while, for, методы строк.py
+++ Условные операторы, циклы while, for, методы строк.py
@@ -163,9 +163,9 @@ num = 10
 for i in range(num):
     print(i)
 
-# num = 10
-# for i in range(num + 1):
-#     print(i)
+num = 10
+for i in range(num + 1):
+    print(i)
 
 # num = 10
 # for i in range(1, num, 3):
