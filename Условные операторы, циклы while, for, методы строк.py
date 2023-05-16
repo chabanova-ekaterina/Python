d8efb118f06b3fb6800273e7131c534ffa9fc47c
ITKaty <117355327+chabanova-ekaterina@users.noreply.github.com>
2023-05-16 11:14:49 +0300

    Update Условные операторы, циклы while, for, методы строк.py

--- Условные операторы, циклы while, for, методы строк.py
+++ Условные операторы, циклы while, for, методы строк.py
@@ -67,12 +67,12 @@ print(w6.isdigit())
 print(w7.isdigit())
 print(w8.isdigit())
 
-# w6 = '01234'
-# w7 = 'QWErty'
-# w8 = '1234K'
-# print(w6.isalpha())
-# print(w7.isalpha())
-# print(w8.isalpha())
+w6 = '01234'
+w7 = 'QWErty'
+w8 = '1234K'
+print(w6.isalpha())
+print(w7.isalpha())
+print(w8.isalpha())
 
 
 # a = int(input('Enter the number '))
